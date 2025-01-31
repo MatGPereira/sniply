962906b1a55a9e14800d97617e27a681b1bd1f14
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2025-01-31 18:07:34 -0300

    refactor: restructure project folders

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { authRoutes } from '@/modules/auth/presentation/routes';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [...authRoutes],
 });
 
-export default router;
+export { router };
---
+++ src/modules/auth/presentation/routes/index.ts
@@ -0,0 +1,5 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+const authRoutes = <RouteRecordRaw[]>[];
+
+export { authRoutes };
