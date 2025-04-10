e8b73a8324afae9b77aedace279eaba1525e1c46
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2025-04-10 19:47:30 -0300

    feat(home): open options for login and register in home page

--- src/common/presentation/components/Aside.vue
+++ src/common/presentation/components/Aside.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import Icon from './Icon.vue';
 import Button from './Button.vue';
+
+const isOptionsOpened = ref<boolean>(false);
+function openSidebarOptions() {
+  isOptionsOpened.value = !isOptionsOpened.value;
+}
+
+const openedOptionsStyle = computed(() => ({
+  bottom: isOptionsOpened.value ? 'calc(var(--space-5) - 3px)' : '-100%',
+  opacity: isOptionsOpened.value ? '1' : '0',
+}));
 </script>
 
 <template>
   <aside>
-    <button type="button">
+    <button
+      type="button"
+      @click="openSidebarOptions"
+    >
       <Icon
         class="c-icon"
         :icon="faBars"
         size="3x"
+        aria-hidden="true"
       />
     </button>
-    <div>
+    <div :style="openedOptionsStyle">
       <Button>Sign up</Button>
       <Button class="transparent-button">Sign in</Button>
     </div>
@@ -42,15 +57,18 @@ aside {
   div {
     background-color: var(--color-lighten-light-dark);
     border-radius: var(--radius-default) var(--radius-default) 0 0;
-    bottom: -50%;
     display: flex;
     flex-direction: column;
     inset-inline: 0;
     padding: var(--space-1);
     position: fixed;
     row-gap: calc(var(--space-1) / 2);
+    transition:
+      bottom var(--transition-duration-default) linear,
+      opacity var(--transition-duration-default) linear
+    ;
+    will-change: bottom, opacity;
     z-index: var(--z-index-below);
-    transition: bottom var(--transition-duration-exit) linear;
 
     .transparent-button {
       background-color: transparent;
